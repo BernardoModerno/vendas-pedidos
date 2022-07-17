cf49a25cf0b6efcc6a0f344756425813b3df4070
Bernardo Moderno <bernardo.moderno@gmail.com>
2022-07-17 13:53:16 -0300

    Paginação Order

--- backend/src/controllers/order/ListOrdersController.ts
+++ backend/src/controllers/order/ListOrdersController.ts
@@ -1,13 +1,44 @@
 import {Request, Response} from 'express'
+import prismaClient from '../../prisma';
 import { ListOrdersService } from '../../services/order/ListOrdersService'
 
 class ListOrdersController{
   async handle(req: Request, res: Response){
-    const listOrdersService = new ListOrdersService()
+    const take = 15 as number;
+    const page = parseInt(req.query.page as string || '1');
+    const total= await prismaClient.order.count(
+      {
+        where:{
+          draft: false,
+          status: false,
+        },
+      }
+    );
 
-    const orders = await listOrdersService.execute();
+    const orders = await prismaClient.order.findMany({
+      where:{
+        draft: false,
+        status: false,
+      },
+      orderBy:{
+        created_at: 'desc'
+      },
+      include:{
+        items : true
+      },
+      take: take,
+      skip: (page - 1) * take,
+    })
 
-    return res.json(orders);
+    //const orders = await listOrdersService.execute();
+
+    return res.json({
+      data: orders,
+      meta: {
+        total,
+        page,
+        last_page: Math.ceil(total / take)}
+    });
 
   }
 }
