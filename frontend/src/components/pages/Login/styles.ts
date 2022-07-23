0be901a6ef96bdadc1a1fd42ff5b0395375cd275
Bernardo Moderno <bernardo.moderno@gmail.com>
2022-07-23 16:33:41 -0300

    Input, Button, Auth page

---
+++ frontend/src/components/pages/Login/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 60vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const ImgLogin = styled.img`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  width: 100px;
+  margin-bottom: 20px;
+`
